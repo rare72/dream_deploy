9c1768754f1873d55df31a15f0c51b79d8d34ef9
Phil Vinson <rare72@users.noreply.github.com>
2016-03-26 18:21:56 -0400

    Add install of Git
    The script needs to also install Git. Reference the following URL's
    
    -#Install Git
    
    Install

--- install_debian.sh
+++ install_debian.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Install Git
+apt-get update
+apt-get install git-core
+git config --global user.name "Phil Vinson"
+git config --global user.email rare72_pv@yahoo.com
+git config --list
+###Do not forget to configure the "~/.gitconfig" file ###
+
+# Install Ansible
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install ansible
+
+exit
