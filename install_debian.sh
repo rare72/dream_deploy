02efc9928bb41f62ed4e01d8b1c9857cad5d87fc
Phil Vinson <rare72@users.noreply.github.com>
2016-03-29 17:28:41 -0400

    Update install_debian.sh
    More changes to the basic script. Helps setup the environment on a node then deploy Ansible.

--- install_debian.sh
+++ install_debian.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 # Update Available Packages
-apt-get update
+apt-get update -yV
 
 # Install Important System Packages
-apt-get install make gcc linux-headers-amd64
+apt-get install vim curl make gcc linux-headers-amd64 binutils python -yV
 
 # Install Git
-apt-get install git-core
+apt-get install git-core -yV
 git config --global user.name "Phil Vinson"
 git config --global user.email rare72_pv@yahoo.com
 git config --list
 ###Do not forget to configure the "~/.gitconfig" file ###
 
 # Install Ansible
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible
+apt-get install software-properties-common python-pip python-dev -yV
+pip install -V ansible
+pip install -V --upgrade setuptools requests ecdsa markupsafe
 
 exit
