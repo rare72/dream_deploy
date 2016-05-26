64f9ab76ee239c1aaef4269e2f34c818bc7b055f
Phil Vinson <rare72@users.noreply.github.com>
2016-05-26 11:20:38 -0400

    pv_why
    why is it not working

--- ansible-controller_install_debian.sh
+++ ansible-controller_install_debian.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-### Set up the directory structure within the enviornment.
+### This Script is used to setup the Ansible Controller and the associated enviornment.
+## Set up the directory structure within the enviornment.
 mkdir -p -m 777 /data/logs
 mkdir -p -m 777 /data/logs/ansible
 mkdir -p -m 777 /data/logs/dhcp
@@ -11,37 +12,41 @@ mkdir -p -m 777 /data/PROJECTS
 mkdir -p -m 777 /data/data_app/keys/bad_archive
 mkdir -p -m 777 /data/data_app/ansible
 
+
 ## Setup Enviornment variables
 ## Create a standard enviornment file
 { date -u ; uname -n ;} > /data/logs/vagrant_box_build_time
 { date -u ; uname -n ;} > /data/logs/MASTER_NODE
 
 ## Update Available Packages
-<<<<<<< HEAD
 apt-get update -yV
-=======
-apt-get update -yV
->>>>>>> origin/master
+
 
 ## Install Important System Packages
 apt-get install vim-tiny curl wget make gcc-4.9-base binutils python build-essential linux-headers-$(uname -r) sysv-rc rcconf -yV
 apt-get install python-pip python-dev python-simplejson python-yaml python-gmpy -yV
 pip install paramiko PyYAML Jinja2 httplib2 six
 
+
 ##Install Ansible
 pip install ansible
+
 
 ## Install Git
 apt-get install git-core -yV
 git config --global user.name "Phil Vinson"
 git config --global user.email rare72_pv@yahoo.com
 git config --list
-### Do not forget to verify the setting's with the "~/.gitconfig" file ###
+### Do not forget to verify the setting's with the "~/.gitconfig" file
+
 
 ## Deploy the dream_deploy repository
 cd /data/PROJECTS
 git clone --verbose https://github.com/rare72/dream_deploy.git
+
+
 ## Post Ansible Install
+
 
 # Install vagrant keys
 curl -Lo /home/root/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
