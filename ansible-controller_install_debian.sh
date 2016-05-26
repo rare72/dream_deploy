52595f397f2d288e93a9298130fc15593360663b
Phil Vinson <rare72@users.noreply.github.com>
2016-05-26 12:55:49 -0400

    Minor Tweaks
    Minor Tweaks to the repo

--- ansible-controller_install_debian.sh
+++ ansible-controller_install_debian.sh
@@ -11,6 +11,7 @@ mkdir -p -m 777 /data/tmp
 mkdir -p -m 777 /data/PROJECTS
 mkdir -p -m 777 /data/data_app/keys/bad_archive
 mkdir -p -m 777 /data/data_app/ansible
+mkdir -p -m 640 /root/.ssh/
 
 
 ## Setup Enviornment variables
@@ -25,7 +26,7 @@ apt-get update -y
 ## Install Important System Packages
 apt-get install vim-tiny curl wget make gcc-4.9-base binutils python build-essential linux-headers-$(uname -r) sysv-rc rcconf -y
 apt-get install python-pip python-dev python-simplejson python-yaml python-gmpy -y
-pip install paramiko PyYAML Jinja2 httplib2 six pycrypto
+pip install paramiko PyYAML Jinja2 httplib2 six pycrypto Markupsafe --upgrade
 
 
 ##Install Ansible
