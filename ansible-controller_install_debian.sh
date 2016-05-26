9cebde5ed201d9333a38a18f7cd06ab1d489cea0
Phil Vinson <rare72_pv@yahoo.com>
2016-05-26 02:52:03 -0400

    random
    random

--- ansible-controller_install_debian.sh
+++ ansible-controller_install_debian.sh
@@ -18,21 +18,21 @@ $projects
 { date -u ; uname -n ;} > /data/logs/MASTER_NODE
 
 ## Update Available Packages
-sudo apt-get update
+ apt-get update
 
 ## Install Important System Packages
-sudo apt-get install vim-tiny curl wget make gcc-4.9-base binutils python build-essential linux-headers-$(uname -r) sysv-rc rcconf -yV
+ apt-get install vim-tiny curl wget make gcc-4.9-base binutils python build-essential linux-headers-$(uname -r) sysv-rc rcconf -yV
 apt-get install python-pip python-dev python-simplejson python-yaml python-gmpy -yV
-sudo pip install paramiko PyYAML Jinja2 httplib2 six
-sudo pip install ansible
+ pip install paramiko PyYAML Jinja2 httplib2 six
+ pip install ansible
 
 
 
 ## Install Git
-sudo apt-get install git-core -yV
-sudo git config --global user.name "Phil Vinson"
-sudo git config --global user.email rare72_pv@yahoo.com
-sudo git config --list
+ apt-get install git-core -yV
+ git config --global user.name "Phil Vinson"
+ git config --global user.email rare72_pv@yahoo.com
+ git config --list
 ### Do not forget to verify the setting's with the "~/.gitconfig" file ###
 
 ## Deploy the dream_deploy repository
@@ -41,14 +41,14 @@ git clone --verbose https://github.com/rare72/dream_deploy.git
 ## Post Ansible Install
 
 # Install vagrant keys
-sudo curl -Lo /home/vagrant/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-sudo curl -Lo /home/vagrant/.ssh/vagrant.pub 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-sudo curl -Lo /home/vagrant/.ssh/vagrant_private 'https://raw.githubusercontent.com/rare72/vagrant/master/keys/vagrant'
+ curl -Lo /home/vagrant/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+ curl -Lo /home/vagrant/.ssh/vagrant.pub 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+ curl -Lo /home/vagrant/.ssh/vagrant_private 'https://raw.githubusercontent.com/rare72/vagrant/master/keys/vagrant'
 
-sudo chmod 0600 /home/vagrant/.ssh/authorized_keys
-sudo chmod 0600 /home/vagrant/.ssh/vagrant_private
-sudo chmod 0600 /home/vagrant/.ssh/vagrant.pub
-sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+ chmod 0600 /home/vagrant/.ssh/authorized_keys
+ chmod 0600 /home/vagrant/.ssh/vagrant_private
+ chmod 0600 /home/vagrant/.ssh/vagrant.pub
+ chown -R vagrant:vagrant /home/vagrant/.ssh
 
 
 exit
