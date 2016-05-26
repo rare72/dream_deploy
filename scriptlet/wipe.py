f58696a59806595aaeeb69a86b025cbe1cc65976
Phil Vinson <rare72_pv@yahoo.com>
2016-05-26 02:38:21 -0400

    random

---
+++ scriptlet/wipe.py
@@ -0,0 +1,13 @@
+# wiper.py
+class Wipe(object):
+    def __repr__(self):
+        return '\n'*1000
+
+wipe = Wipe()
+
+### Example Usage
+##Then you can do this from the interpreter all you like :)
+# >>> from wiper import wipe
+# >>> wipe
+# >>> wipe
+# >>> wipe
